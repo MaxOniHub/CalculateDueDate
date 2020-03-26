f105b8b15e20f460d9f31f5370dc30ce3432bb25
MaxOniHub <onyshchenko.max@gmail.com>
2020-03-26 11:11:30 +0200

    - formatted code

--- controllers/ToolsController.js
+++ controllers/ToolsController.js
@@ -2,10 +2,10 @@ var DueDate = require('../models/DueDate')
 const DueDateService = require('../services/DueDateService')
 
 exports.calculateDueDate = function(req, res) {
-  const dueDateModel = new DueDate(req.body.date, req.body.turnaroundTime)
-  const dueDateService = new DueDateService
+  const dueDateModel = new DueDate(req.body.date, req.body.turnaroundTime);
+  const dueDateService = new DueDateService;
 
-  const result = dueDateService.calculateDueDate(dueDateModel)
+  const result = dueDateService.calculateDueDate(dueDateModel);
 
   res.send(result +"");
 };
